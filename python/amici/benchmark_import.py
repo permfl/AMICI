4ee11e8ba80b5dbfbb3ad55962363715ffbcfcdf
yannikschaelte <yannik.schaelte@gmail.com>
2018-09-26 14:19:24 +0200

    add some comments

--- python/amici/benchmark_import.py
+++ python/amici/benchmark_import.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sp
+import h5py
 
 
 class DataProvider:
@@ -23,8 +24,10 @@ class DataProvider:
         pass
     
     @staticmethod
-    def convert_to_h5(model_name):
+    def convert_benchmark_to_h5(model_name):
         """
+        Convert the data format in the benchmark study by Loos et al. to
+        hdf5.
 
         Parameters
         ----------
@@ -32,4 +35,20 @@ class DataProvider:
         model: str
             e.g. "Fujita_SciSignal2010", file name relative to current
             working directory
+
+        Returns
+        -------
+
+        dp: DataProvider
+            a DataProvider to the generated h5_file
         """
+
+        # check if h5 file exists already, then error
+
+        # observables
+
+        # fixed parameters
+
+        # timepoints
+
+        # ...
